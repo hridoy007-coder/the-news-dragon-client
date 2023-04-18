fa92ac888272527f669cf488bd69a0ef088119e5
hridoy007-coder <hridoykarmokar882@gmail.com>
2023-04-18 21:03:39 +0600

    localhost data load

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -7,7 +7,7 @@ import Marquee from "react-fast-marquee";
 const Header = () => {
     return (
         <Container>
-            <div className="text-center">
+            <div className="text-center mt-4">
                 <img src={logo} alt="" />
                 <p className='text-secondary'><small>Journalism Without Fear or Favour</small></p>
                 <p>{moment().format("dddd, MMMM D, YYYY, h:mm:ss a")}</p>
--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const LeftNav = () => {
+    const [catagories, setCatagories] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/catagories')
+            .then(res => res.json())
+            .then(data => console.log(data))
+            .catch(error => console.error(error))
+    }, [])
+
     return (
         <div>
             <h2>LeftNav</h2>
