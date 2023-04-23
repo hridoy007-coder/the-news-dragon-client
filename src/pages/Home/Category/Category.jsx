c5ffc4ef0683aa72961066d678b1441173592011
hridoy007-coder <hridoykarmokar882@gmail.com>
2023-04-24 02:10:49 +0600

    category set

---
+++ src/pages/Home/Category/Category.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useLoaderData, useParams } from 'react-router-dom';
+
+const Category = () => {
+    const { id } = useParams();
+    const categoryNews = useLoaderData();
+    return (
+        <div>
+            <h2>This category News: {categoryNews.length}</h2>
+        </div>
+    );
+};
+
+export default Category;
