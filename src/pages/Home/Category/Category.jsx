1248729b6feaeef9784c97892c23eb18d38a6ed8
hridoy007-coder <hridoykarmokar882@gmail.com>
2023-04-24 02:59:54 +0600

    news card set

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
+import NewsCard from '../NewsCard/NewsCard';
 
 const Category = () => {
     const { id } = useParams();
@@ -7,6 +8,12 @@ const Category = () => {
     return (
         <div>
             <h2>This category News: {categoryNews.length}</h2>
+            {
+                categoryNews.map(news => <NewsCard
+                key= {news._id}
+                news={news}
+                ></NewsCard>)
+            }
         </div>
     );
 };
